addfb03c55a7da42a37a119b76ca924b1596becb
kirils_savinihs <kirills111@gmail.com>
2018-09-20 20:40:10 +0300

    Initial Commit

---
+++ src/main/java/com/stockAnalyzer/stockDataGetter.java
@@ -0,0 +1,11 @@
+package com.stockAnalyzer;
+
+//Class that gets stock data from yahoo finance
+// TODO: write the code
+
+public class stockDataGetter {
+
+    public static void main(String[] args) {
+
+    }
+}
