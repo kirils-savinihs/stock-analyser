959bc6a7fe30b0af2d8de6ee24c6bf213622178f
Kirils_Savinihs <kirills111@gmail.com>
2018-09-27 19:53:29 +0300

    Added add function Renamed some functions

--- src/main/java/com/stockAnalyzer/database/DatabaseManager.java
+++ src/main/java/com/stockAnalyzer/database/DatabaseManager.java
@@ -1,13 +1,10 @@
 package com.stockAnalyzer.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseManager {
 
-    public Connection con;
+    private Connection con;
 
     public DatabaseManager() {
         try {
@@ -22,6 +19,105 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Adds a new CompanyStockData object's fields to the mysql database
+     *
+     * @param symbol Company symbol e.g "INTC" for Intel Corporation
+     */
+    public void add(String symbol) {
+        CompanyStockData data = new CompanyStockData(symbol);
+        Statement st = null;
+        try {
+            st = con.createStatement();
+            String sql = "INSERT INTO `stock_database`.`stocks`\n" +
+                    "(`id`,\n" +
+                    "`symbol`,\n" +
+                    "`PeRatio`,\n" +
+                    "`Sector`,\n" +
+                    "`PriceToBook`,\n" +
+                    "`PriceToSales`,\n" +
+                    "`DividendYield`,\n" +
+                    "`ReturnOnEquity`,\n" +
+                    "`ReturnOnAssets`,\n" +
+                    "`ProfitMargin`,\n" +
+                    "`TtmEPS`,\n" +
+                    "`Debt`,\n" +
+                    "`currentAssets`,\n" +
+                    "`currentDebt`,\n" +
+                    "`totalDebt`,\n" +
+                    "`shareHolderEquity`,\n" +
+                    "`cashFlow`)\n" +
+                    "VALUES\n" +
+                    "(null,\n\'" +
+                    data.getSymbol() + "\',\n" +
+                    data.PeRatio + ",\n\'" +
+                    data.Sector + "\',\n" +
+                    data.PriceToBook + ",\n" +
+                    data.PriceToSales + ",\n" +
+                    data.DividendYield + ",\n" +
+                    data.ReturnOnEquity + ",\n" +
+                    data.ReturnOnAssets + ",\n" +
+                    data.ProfitMargin + ",\n" +
+                    data.TtmEPS + ",\n" +
+                    data.Debt + ",\n" +
+                    data.currentAssets + ",\n" +
+                    data.currentDebt + ",\n" +
+                    data.totalDebt + ",\n" +
+                    data.shareHolderEquity + ",\n" +
+                    data.cashFlow + ");\n";
+
+            st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     * Resets the database by dropping and recreating everything
+     */
+    public void resetDatabase() {
+        String[] sql = new String[5];
+
+        sql[0] = "drop database if exists stock_database;";
+        sql[1] = "create database stock_database;";
+        sql[2] = "use stock_database;";
+        sql[3] = "drop table if exists stocks;";
+        sql[4] = "create table stocks (\n" +
+                "id int (10) unsigned not null auto_increment,\n" +
+                "symbol varchar(30),\n" +
+                "PeRatio double,\n" +
+                "Sector varchar(30),\n" +
+                "PriceToBook  double,\n" +
+                "PriceToSales double,\n" +
+                "DividendYield\tdouble,\n" +
+                "ReturnOnEquity double,\n" +
+                "ReturnOnAssets double,\n" +
+                "ProfitMargin double,\n" +
+                "TtmEPS double,\n" +
+                "Debt bigint,\n" +
+                "currentAssets bigint, \n" +
+                "currentDebt bigint,\n" +
+                "totalDebt bigint,\n" +
+                "shareHolderEquity bigint,\n" +
+                "cashFlow bigint,\n" +
+                "primary key (id)\n" +
+                ");";
+        try {
+            Statement st = con.createStatement();
+            for (String s : sql) {
+                st.execute(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Closes the connection
+     */
     public void closeConnection() {
         try {
             con.commit();
@@ -29,64 +125,60 @@ public class DatabaseManager {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     /**
      * Finds stock by id
      *
      * @param id stock id in database
-     * @return Stock object
+     * @return CompanyStockData object
      */
-    public Stock findStock(int id) {
-        Stock stock = null;
+    public CompanyStockData findById(int id) {
+        CompanyStockData companyStockData = null;
         try {
-            java.sql.Statement st = con.createStatement();
+            Statement st = con.createStatement();
             ResultSet rs = st.executeQuery("select * from stock_database.stocks where id=" + id);
             rs.next();
-            stock = new Stock(id, rs.getString(2));
+            companyStockData = new CompanyStockData(id, rs.getString(2));
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return stock;
+        return companyStockData;
     }
 
     /**
      * Finds stock by symbol example: "INTC" for Intel Corporation
      * @param symbol stock symbol
-     * @return Stock object or null if sqlexception occurs
+     * @return CompanyStockData object or null if sqlexception occurs
      */
 
-    public Stock findStock(String symbol)
-    {
-        Stock stock = null;
+    public CompanyStockData findBySymbol(String symbol) {
+        CompanyStockData companyStockData = null;
         try {
             java.sql.Statement st = con.createStatement();
             System.out.println("select * from stock_database.stocks where symbol=\""+symbol+"\";");
             ResultSet rs = st.executeQuery("select * from stock_database.stocks where symbol=\""+symbol+"\"");
             rs.next();
-            stock = new Stock(rs.getInt(1), rs.getString(2));
+            companyStockData = new CompanyStockData(rs.getInt(1), rs.getString(2));
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return stock;
+        return companyStockData;
     }
 
 
 
     public static void main(String[] args) {
         //For testing purposes only
+
         DatabaseManager db = new DatabaseManager();
-
-
-        Stock st2 = db.findStock("INTC");
-
-
-        System.out.println(st2.getId()+st2.getSymbol());
-
-
-
+//        CompanyStockData st2 = db.findBySymbol("INTC");
+//        System.out.println(st2.getId()+st2.getSymbol());
+        db.resetDatabase();
+        db.add("INTC");
+        db.closeConnection();
     }
+
 
 
 }
