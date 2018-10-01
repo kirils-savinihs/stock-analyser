25d3bab086e427395054ad5298e33c2451ac93af
kirils-savinihs <kirills111@gmail.com>
2018-10-01 20:55:05 +0300

    Implemented Comparable, added compare method

--- src/main/java/com/stockAnalyzer/database/CompanyStockData.java
+++ src/main/java/com/stockAnalyzer/database/CompanyStockData.java
@@ -1,5 +1,6 @@
 package com.stockAnalyzer.database;
 
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import pl.zankowski.iextrading4j.api.stocks.*;
 import pl.zankowski.iextrading4j.client.IEXTradingClient;
 import pl.zankowski.iextrading4j.client.rest.request.stocks.CompanyRequestBuilder;
@@ -9,22 +10,22 @@ import pl.zankowski.iextrading4j.client.rest.request.stocks.QuoteRequestBuilder;
 
 import java.math.BigDecimal;
 
-public class CompanyStockData {
+public class CompanyStockData implements Comparable<CompanyStockData> {
 
     private Quote quote;
     private Company company;
     private KeyStats keystats;
     private Financial financials;
-    public final BigDecimal PeRatio;
+    public final BigDecimal PeRatio;//less is better
     public final String Sector;
-    public final BigDecimal PriceToBook;
-    public final BigDecimal PriceToSales;
-    public final BigDecimal DividendYield;
-    public final BigDecimal ReturnOnEquity;
-    public final BigDecimal ReturnOnAssets;
-    public final BigDecimal ProfitMargin;
+    public final BigDecimal PriceToBook;//less is better
+    public final BigDecimal PriceToSales;//less is better
+    public final BigDecimal DividendYield;//more is better
+    public final BigDecimal ReturnOnEquity;//more is better
+    public final BigDecimal ReturnOnAssets;//more is better
+    public final BigDecimal ProfitMargin;//more is better
     public final BigDecimal TtmEPS;
-    public final BigDecimal Debt;
+    public final BigDecimal Debt; //more is worse
     public final BigDecimal currentAssets;
     public final BigDecimal currentDebt;
     public final BigDecimal totalDebt;
@@ -32,7 +33,7 @@ public class CompanyStockData {
     public final BigDecimal cashFlow;
     public final BigDecimal PriceToCashFlow;
     public final BigDecimal DebtToEquity;
-    public final BigDecimal Liquidity;
+    public final BigDecimal Liquidity;//more is better
     public final BigDecimal LatestPrice;
 
 
@@ -129,7 +130,7 @@ public class CompanyStockData {
         this.cashFlow = financials.getCashFlow();
 
         if (totalDebt == null || shareHolderEquity == null)
-            this.DebtToEquity = new BigDecimal(0);
+            this.DebtToEquity = null;
         else
             this.DebtToEquity = totalDebt.divide(shareHolderEquity, 4, BigDecimal.ROUND_HALF_UP);
 
@@ -137,15 +138,14 @@ public class CompanyStockData {
 
 
         if (this.LatestPrice == null || this.cashFlow == null)
-            this.PriceToCashFlow = new BigDecimal(0);
+            this.PriceToCashFlow = null;
         else
             this.PriceToCashFlow = this.LatestPrice.divide(this.cashFlow.divide(keystats.getSharesOutstanding(), 2, BigDecimal.ROUND_HALF_UP), 2, BigDecimal.ROUND_HALF_UP);
 
         if (this.currentAssets == null || this.currentDebt == null)
-            this.Liquidity = new BigDecimal(0);
+            this.Liquidity = null;
         else
             this.Liquidity = currentAssets.divide(currentDebt, 2, BigDecimal.ROUND_HALF_UP);
-
 
 
     }
@@ -153,7 +153,7 @@ public class CompanyStockData {
     @Override
     public String toString() {
         return "CompanyStockData{" +
-                ". PeRatio=" + PeRatio +
+                "\n PeRatio=" + PeRatio +
                 "\n Sector=" + Sector +
                 "\n PriceToBook=" + PriceToBook +
                 "\n PriceToSales=" + PriceToSales +
@@ -177,5 +177,133 @@ public class CompanyStockData {
                 "}\n";
     }
 
-	
+
+    @Override
+    public int compareTo(CompanyStockData other) {
+        // return >0 if this is better , return <0 if this is worse
+
+
+        double[] compPercentages = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        double division = 0;
+
+        try {
+            //PeRation: less is better
+            division = this.PeRatio.divide(other.PeRatio, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[0] = (1 - division);
+            else
+                compPercentages[0] = (division - 1) * -1;
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+
+        //PriceToBook: less is better
+        try {
+            division = this.PriceToBook.divide(other.PriceToBook, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[1] = (1 - division);
+            else
+                compPercentages[1] = (division - 1) * -1;
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+
+        try {
+            //PriceToSales: less is better
+            division = this.PriceToSales.divide(other.PriceToSales, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[2] = (1 - division);
+            else
+                compPercentages[2] = (division - 1) * -1;
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+
+        //DividendYield: more is better
+        try {
+            division = this.DividendYield.divide(other.DividendYield, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[3] = (1 - division) * -1;
+            else
+                compPercentages[3] = (division - 1);
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+        //ReturnOnEquity: more is better
+
+        try {
+            division = this.ReturnOnEquity.divide(other.ReturnOnEquity, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[4] = (1 - division) * -1;
+            else
+                compPercentages[4] = (division - 1);
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+        //ReturnOnAssets: more is better
+        try {
+            division = this.ReturnOnAssets.divide(other.ReturnOnAssets, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[5] = (1 - division) * -1;
+            else
+                compPercentages[5] = (division - 1);
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+
+        //ProfitMargin: more is better
+        try {
+            division = this.ProfitMargin.divide(other.ProfitMargin, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[6] = (1 - division) * -1;
+            else
+                compPercentages[6] = (division - 1);
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+        //Debt: less is better
+        try {
+            division = this.Debt.divide(other.Debt, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[0] = (1 - division);
+            else
+                compPercentages[0] = (division - 1) * -1;
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+        //Liquidity: more is better
+        try {
+            division = this.Liquidity.divide(other.Liquidity, 3, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (division < 1)
+                compPercentages[6] = (1 - division) * -1;
+            else
+                compPercentages[6] = (division - 1);
+
+        } catch (NullPointerException e) {
+        } catch (ArithmeticException e) {
+        }
+
+        double sum = 0;
+
+        for (double i : compPercentages)
+            sum += i;
+
+        System.out.println(sum);
+
+        if (sum > 0)
+            return 1;
+        else if (sum < 0)
+            return -1;
+        else
+            return 0;
+
+    }
 }
